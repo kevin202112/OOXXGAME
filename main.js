a0c7213ccb587881eaf5594080cb0343aa3fcac7
kevin202108 <88997988+kevin202108@users.noreply.github.com>
2022-09-18 12:36:29 +0800

    clear change

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function btn(r,c){
 function clear(){
     for(var r = 0; r < 3; r ++){
         for(var c = 0; c < 3; c ++){
-            document.getElementById("btn"+r+c).innerHTML = " ";
+            document.getElementById("btn"+r+c).innerHTML = "click me";
         }
     }
 }
